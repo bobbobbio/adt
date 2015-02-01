1da326cd2f353f6159cfd75ec952007b2bb851ad
Remi Bernotavicius <remi@kunugiken.com>
2015-01-31 18:55:20 -0800

    Added comments to http_server

--- prog/http_server.c
+++ prog/http_server.c
@@ -7,6 +7,8 @@
 static void
 client_404(struct socket *client_conn)
 {
+   // Send 404 to the given client.  It includes a small body with a header
+   // saying 'Not Found'
    const struct string *req =
       strw("HTTP/1.0 404 Not Found\n\n<html><h1>Not Found</h1></html>");
    ecrash(socket_write(client_conn, req));
@@ -15,13 +17,16 @@ client_404(struct socket *client_conn)
 static void
 serve_client(struct socket *client_conn, struct inet_addr *client_addr)
 {
+   // Read off only the first line of the request.
    create(string, buff);
    create_line_reader(client_lr, socket_to_stream(client_conn));
    line_reader_get_line(&client_lr, &buff);
 
+   // Log the request
    aprintf("%s -- \"%s\"\n",
       print(inet_addr, client_addr), print(string, &buff));
 
+   // We only support GET, if it doesn't match the get request, we send a 404
    create_regex(get_reg, strw("GET /([^ ]*) HTTP.*"));
    create(string, path);
    if (!regex_match(&get_reg, &buff, &path)) {
@@ -29,6 +34,8 @@ serve_client(struct socket *client_conn, struct inet_addr *client_addr)
       return;
    }
 
+   // Look for the path they requested in the current working directory.  If we
+   // couldn't open the path because it doesn't exist, send 404.
    create(file, file);
    ehandle(error, file_open(&file, &path, 0)) {
       if (error_is_type(error, file_not_found_error)) {
@@ -39,6 +46,7 @@ serve_client(struct socket *client_conn, struct inet_addr *client_addr)
       }
    }
 
+   // Otherwise we read the file and write the contents back to the client.
    ecrash(file_read(&file, &buff));
    ecrash(socket_write(client_conn, strw("HTTP/1.0 200 OK\n\n")));
    ecrash(socket_write(client_conn, &buff));
@@ -47,13 +55,17 @@ serve_client(struct socket *client_conn, struct inet_addr *client_addr)
 int
 main(void)
 {
+   // Listen on port 8080 for incoming connections.
    create(socket, tcp_socket);
    ecrash(socket_bind(8080, &tcp_socket));
+   // The second argument here is how many connections to queue in the kernel
+   // before dropping connections.
    ecrash(socket_listen(&tcp_socket, 10));
 
    printf("HTTP server started on port 8080\n");
 
    while (true) {
+      // Accept a connection and serve it's request synchronously.
       create(socket, client_conn);
       create(inet_addr, client_addr);
       ecrash(socket_accept(&tcp_socket, &client_conn, &client_addr));
